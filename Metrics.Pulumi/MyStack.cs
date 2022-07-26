efb7bf3fbd942b1bb4cadf3e397f368c7c1b2c65
Simon Foster <funkysi1701@gmail.com>
2022-07-26 21:39:43 +0100

    add pulumi

---
+++ Metrics.Pulumi/MyStack.cs
@@ -0,0 +1,19 @@
+﻿using Pulumi;
+using Pulumi.Azure.AppInsights;
+using Pulumi.Azure.AppService;
+using Pulumi.Azure.AppService.Inputs;
+using Pulumi.Azure.Core;
+
+namespace Metrics.Pulumi
+{
+    public class MyStack : Stack
+    {
+        public MyStack()
+        {
+            var resourceGroup = new ResourceGroup("blog-pulumi", new ResourceGroupArgs
+            {
+                Name = "metrics-function-pulumi",
+            });
+        }
+    }
+}
---
+++ Metrics.Pulumi/Program.cs
@@ -0,0 +1,10 @@
+﻿using Pulumi;
+using System.Threading.Tasks;
+
+namespace Metrics.Pulumi
+{
+    public class Program
+    {
+        private static Task<int> Main() => Deployment.RunAsync<MyStack>();
+    }
+}
