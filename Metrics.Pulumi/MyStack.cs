266bf5f11ad8dfafe667ef567da52d36f34e0486
Simon Foster <sfoster@bluetreegroup.co.uk>
2022-08-12 12:43:04 +0100

    create function and timerfunction

--- Metrics.Pulumi/MyStack.cs
+++ Metrics.Pulumi/MyStack.cs
@@ -54,7 +54,125 @@ namespace Metrics.Pulumi
                 ResourceGroupName = resourceGroup.Name,
             });
 
-            var app = new WebApp("app", new WebAppArgs
+            var timerfunction = new WebApp("app", new WebAppArgs
+            {
+                Name = $"metrics-pulumi-timerfunction-{config.Require("env")}",
+                Kind = "FunctionApp",
+                ResourceGroupName = resourceGroup.Name,
+                ServerFarmId = appServicePlan.Id,
+                SiteConfig = new SiteConfigArgs
+                {
+                    AppSettings = new[]
+                    {
+                        new NameValuePairArgs{
+                            Name = "AzureWebJobsStorage",
+                            Value = GetConnectionString(resourceGroup.Name, storageAccount.Name),
+                        },
+                        new NameValuePairArgs{
+                            Name = "FUNCTIONS_WORKER_RUNTIME",
+                            Value = "dotnet",
+                        },
+                        new NameValuePairArgs{
+                            Name = "TWConsumerKey",
+                            Value = config.RequireSecret("TWConsumerKey"),
+                        },
+                        new NameValuePairArgs{
+                            Name = "TWConsumerSecret",
+                            Value = config.RequireSecret("TWConsumerSecret"),
+                        },
+                        new NameValuePairArgs{
+                            Name = "TWAccessToken",
+                            Value = config.RequireSecret("TWAccessToken"),
+                        },
+                        new NameValuePairArgs{
+                            Name = "TWAccessSecret",
+                            Value = config.RequireSecret("TWAccessSecret"),
+                        },
+                        new NameValuePairArgs{
+                            Name = "GitHubToken",
+                            Value = config.RequireSecret("GitHubToken"),
+                        },
+                        new NameValuePairArgs{
+                            Name = "Username1",
+                            Value = "funkysi1701",
+                        },
+                        new NameValuePairArgs{
+                            Name = "DEVTOAPI",
+                            Value = config.RequireSecret("DEVTOAPI"),
+                        },
+                        new NameValuePairArgs{
+                            Name = "DEVTOURL",
+                            Value = "https://dev.to/api/",
+                        },
+                        new NameValuePairArgs{
+                            Name = "RSSFeed",
+                            Value = "https://www.funkysi1701.com/index.xml",
+                        },
+                        new NameValuePairArgs{
+                            Name = "OPSAPI",
+                            Value = config.RequireSecret("OPSAPI"),
+                        },
+                        new NameValuePairArgs{
+                            Name = "OPSURL",
+                            Value = "https://community.ops.io/api/",
+                        },
+                        new NameValuePairArgs{
+                            Name = "OctopusKey",
+                            Value = config.RequireSecret("OctopusKey"),
+                        },
+                        new NameValuePairArgs{
+                            Name = "OctopusElecMPAN",
+                            Value = config.RequireSecret("OctopusElecMPAN"),
+                        },
+                        new NameValuePairArgs{
+                            Name = "OctopusElecSerial",
+                            Value = config.RequireSecret("OctopusElecSerial"),
+                        },
+                        new NameValuePairArgs{
+                            Name = "OctopusGasMPAN",
+                            Value = config.RequireSecret("OctopusGasMPAN"),
+                        },
+                        new NameValuePairArgs{
+                            Name = "OctopusGasSerial",
+                            Value = config.RequireSecret("OctopusGasSerial"),
+                        },
+                        new NameValuePairArgs{
+                            Name = "DatabaseName",
+                            Value = $"Metrics{config.Require("env")}",
+                        },
+                        new NameValuePairArgs{
+                            Name = "OldRSSFeed",
+                            Value = "https://www.pwnedpass.com/feed/",
+                        },
+                        new NameValuePairArgs{
+                            Name = "ConnectionString",
+                            Value = config.RequireSecret("ConnectionString"),
+                        },
+                        new NameValuePairArgs{
+                            Name = "CollectionName",
+                            Value = $"Metrics{config.Require("env")}",
+                        },
+                        new NameValuePairArgs{
+                            Name = "runtime",
+                            Value = "dotnet",
+                        },
+                        new NameValuePairArgs{
+                            Name = "APPINSIGHTS_INSTRUMENTATIONKEY",
+                            Value = appInsights.InstrumentationKey,
+                        },
+                        new NameValuePairArgs{
+                            Name = "APPLICATIONINSIGHTS_CONNECTION_STRING",
+                            Value = Output.Format($"InstrumentationKey={appInsights.InstrumentationKey};IngestionEndpoint=https://westeurope-1.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/"),
+                        },
+                        new NameValuePairArgs{
+                            Name = "FUNCTIONS_EXTENSION_VERSION",
+                            Value = "~4",
+                        },
+                    },
+                },
+            });
+
+            var function = new WebApp("app", new WebAppArgs
             {
                 Name = $"metrics-pulumi-function-{config.Require("env")}",
                 Kind = "FunctionApp",
@@ -63,7 +181,7 @@ namespace Metrics.Pulumi
                 SiteConfig = new SiteConfigArgs
                 {
                     AppSettings = new[]
-                    {
+        {
                         new NameValuePairArgs{
                             Name = "AzureWebJobsStorage",
                             Value = GetConnectionString(resourceGroup.Name, storageAccount.Name),
