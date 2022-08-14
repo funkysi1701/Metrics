d748a95f1381ddfcbf84a300e80a16f1aee27c7f
Simon Foster <funkysi1701@gmail.com>
2022-08-14 22:55:32 +0100

    tweak config

--- Metrics.Pulumi/MyStack.cs
+++ Metrics.Pulumi/MyStack.cs
@@ -115,6 +115,14 @@ namespace Metrics.Pulumi
                             Value = GetConnectionString(resourceGroup.Name, storageAccount.Name),
                         },
                         new NameValuePairArgs{
+                            Name = "WEBSITE_CONTENTAZUREFILECONNECTIONSTRING",
+                            Value = GetConnectionString(resourceGroup.Name, storageAccount.Name),
+                        },
+                        new NameValuePairArgs{
+                            Name = "WEBSITE_CONTENTSHARE",
+                            Value = $"metrics-pulumi-timerfunction-{config.Require("env")}-091999e2",
+                        },
+                        new NameValuePairArgs{
                             Name = "FUNCTIONS_WORKER_RUNTIME",
                             Value = "dotnet",
                         },
@@ -201,10 +209,6 @@ namespace Metrics.Pulumi
                         new NameValuePairArgs{
                             Name = "runtime",
                             Value = "dotnet",
-                        },
-                        new NameValuePairArgs{
-                            Name = "APPINSIGHTS_INSTRUMENTATIONKEY",
-                            Value = appInsights.InstrumentationKey,
                         },
                         new NameValuePairArgs{
                             Name = "APPLICATIONINSIGHTS_CONNECTION_STRING",
