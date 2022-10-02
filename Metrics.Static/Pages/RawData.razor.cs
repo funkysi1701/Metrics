3f0b48b25a2314525894e1ada23a66845ec1a987
Simon Foster <funkysi1701@gmail.com>
2022-10-02 22:16:12 +0100

    filters
    tweak padding

--- Metrics.Static/Pages/RawData.razor.cs
+++ Metrics.Static/Pages/RawData.razor.cs
@@ -3,6 +3,7 @@ using Metrics.Core.Enum;
 using Metrics.Core.Model;
 using Metrics.Static.Services;
 using Microsoft.AspNetCore.Components;
+using System.Reflection.Metadata;
 
 namespace Metrics.Static.Pages
 {
@@ -13,9 +14,37 @@ namespace Metrics.Static.Pages
 
         protected IList<IList<ChartViewWithType>> chartViews;
         protected List<IList<IList<ChartViewWithType>>> listoflists;
+        protected MetricType SelectedType { get; set; }
+        protected List<MetricType> types { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
+            types = new List<MetricType>
+            {
+                MetricType.TwitterFollowers,
+                MetricType.TwitterFollowing,
+                MetricType.NumberOfTweets,
+                MetricType.TwitterFavourites,
+                MetricType.GitHubFollowers,
+                MetricType.GitHubFollowing,
+                MetricType.GitHubRepo,
+                MetricType.GitHubStars,
+                MetricType.GitHubCommits,
+                MetricType.DevToPosts,
+                MetricType.DevToPublishedPosts,
+                MetricType.DevToViews,
+                MetricType.DevToReactions,
+                MetricType.DevToComments,
+                MetricType.Gas,
+                MetricType.Electricity,
+                MetricType.Blog,
+                MetricType.OldBlog,
+                MetricType.OPSPosts,
+                MetricType.OPSPublishedPosts,
+                MetricType.OPSViews,
+                MetricType.OPSReactions,
+                MetricType.OPSComments
+            };
             await Load();
         }
 
@@ -28,6 +57,14 @@ namespace Metrics.Static.Pages
                 listoflists.Add(chartViews);
                 await AppInsights.TrackEvent($"LoadRawData MetricType: {i}, OffSet: 0, User: funkysi1701");
             }
+        }
+
+        protected async Task Refresh()
+        {
+            listoflists = new List<IList<IList<ChartViewWithType>>>();
+            chartViews = await BlogService.GetChart((int)SelectedType, (int)MyChartType.Hourly, 0, "funkysi1701");
+            listoflists.Add(chartViews);
+            await AppInsights.TrackEvent($"LoadRawData MetricType: {(int)SelectedType}, OffSet: 0, User: funkysi1701");
         }
 
         public void RefreshMe()
