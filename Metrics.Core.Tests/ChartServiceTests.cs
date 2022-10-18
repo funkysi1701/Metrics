800b3c58f817684d94c73de86335dd891c6de681
Simon Foster <funkysi1701@gmail.com>
2022-10-18 08:10:48 +0100

    add tests

--- Metrics.Core.Tests/ChartServiceTests.cs
+++ Metrics.Core.Tests/ChartServiceTests.cs
@@ -40,5 +40,30 @@ namespace Metrics.Core.Tests
 
             Assert.NotNull(response);
         }
+
+        [Fact]
+        public async Task Get_RetunsError()
+        {
+            var mockHttpMessageHandler = new Mock<HttpMessageHandler>();
+            var res = new List<IList<ChartViewWithType>>();
+            var bpp = JsonConvert.SerializeObject(res);
+            mockHttpMessageHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(), ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync(new HttpResponseMessage { StatusCode = HttpStatusCode.BadRequest, Content = new StringContent(bpp) });
+            var http = new HttpClient(mockHttpMessageHandler.Object);
+            var inMemorySettings = new Dictionary<string, string> {
+                {"BaseURL", "http://example.com"},
+            };
+
+            IConfiguration config = new ConfigurationBuilder()
+                .AddInMemoryCollection(inMemorySettings)
+                .Build();
+            var insights = new Mock<IApplicationInsights>();
+            var chart = new ChartService(http, config, insights.Object);
+
+            var response = await chart.Get(0, 0, 0, "funkysi1701");
+
+            Assert.Null(response);
+        }
     }
 }
