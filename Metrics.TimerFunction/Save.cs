a7430fb4a53f9a82f1c16ddf080da7f40417f6db
Simon Foster <sfoster@bluetreegroup.co.uk>
2022-11-06 09:12:57 +0000

    fix sched

--- Metrics.TimerFunction/Save.cs
+++ Metrics.TimerFunction/Save.cs
@@ -89,7 +89,7 @@ namespace Metrics.TimerFunction
         }
 
         [FunctionName("SaveMastodonFollowers")]
-        public async Task Run14([TimerTrigger("0 * * * * *", RunOnStartup = false)] TimerInfo myTimer, ILogger log, ExecutionContext context)
+        public async Task Run14([TimerTrigger("0 59 * * * *", RunOnStartup = false)] TimerInfo myTimer, ILogger log, ExecutionContext context)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
             foreach (var user in twusers)
