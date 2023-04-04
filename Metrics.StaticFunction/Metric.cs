5ceee9193c5896c1352f3f6f45b3663012533995
Simon Foster <simon.foster@optix.co.uk>
2023-04-04 09:09:55 +0100

    metrics endpoint

---
+++ Metrics.StaticFunction/Metric.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace Metrics.StaticFunction
+{
+    public class Metric
+    {
+        private readonly IConfiguration Configuration;
+
+        public Metric(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        [FunctionName("Metrics")]
+        public async Task<IActionResult> Metrics(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            return new OkObjectResult("testing");
+        }
+    }
+}
